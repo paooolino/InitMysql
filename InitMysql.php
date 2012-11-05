cd10ed62d93b3b5d871bd1a65153fdca950c2c3e
Paolo Savoldi <paooolino@gmail.com>
2012-11-05 14:07:45 +0100

    moving usage notes to readme file

--- InitMysql.php
+++ InitMysql.php
@@ -1,46 +1,11 @@
 <?php
 /*
-	InitMysql
-		A PHP class that helps in MySQL database tables creation
-		
-	Paolo Savoldi 2012
-	paooolino@gmail.com
-	
-	USAGE
-	
-		1) Include InitMysql
-			
-			> include("InitMysql.php");
-			
-		2) Have a database connection
-			
-			> mysql_connect("localhost","root","");
-			> mysql_select_db("testDB");		
-			
-		3) Create initier object
-			
-			> $initier = new InitMysql();
-			
-		4) Let's create tables!
-			
-			> $initier -> createTable("lessons", "name");
-			> $initier -> createTable("slides", "id_lesson:EXT>lessons, title, text:TEXT");
-			
-	METHODS
-	
-		createTable(<string: tableName>, <string: fields>);
-		
-		Creates a new table.
-		
-		<string: tableName> 	The table name.
-		
-		<string: fields> 		A list of field name separed by comma in the following format:
-									<field name>[:<field type>]
-									If not specified, field type is assumed to be "NORMAL" (VARCHAR 255).
-									Other types are defined in the parts array (see the constructor code).
-									"ID", "created" and "order" fields are automatically added.
-		
-*/
+ *	InitMysql
+ *		A PHP class that helps in MySQL database tables creation
+ *		
+ *	Paolo Savoldi 2012
+ *	paooolino@gmail.com		
+ */
 class InitMysql{
 	private $parts = array();
 	
